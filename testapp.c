78cd426f088af0ca7a4c186711de87790203c5f1
sachinites <sachinites@gmail.com>
2019-11-16 11:09:13 -0800

    Added 3 node linear topology.

--- testapp.c
+++ testapp.c
@@ -43,6 +43,7 @@ extern graph_t *build_simple_l2_switch_topo();
 extern graph_t *build_square_topo();
 extern graph_t *build_linear_topo();
 extern graph_t *build_dualswitch_topo();
+extern graph_t *linear_3_node_topo();
 extern void nw_init_cli();
 
 graph_t *topo = NULL;
@@ -51,7 +52,7 @@ int
 main(int argc, char **argv){
 
     nw_init_cli();
-    topo = build_first_topo();
+    topo = linear_3_node_topo();
     start_shell(); 
     return 0;
 }
--- topologies.c
+++ topologies.c
@@ -349,3 +349,41 @@ build_dualswitch_topo(){
     return topo;
 }
 
+
+graph_t *
+linear_3_node_topo(){
+
+#if 0
+
+                                        +---------|                                  +----------+
++--------+                              |         |                                  |R3        |
+|R1      |eth0/1                  eth0/2|R2       |eth0/3                      eth0/4|122.1.1.3 |
+|122.1.1.1+-----------------------------+122.1.1.2|+----------------------------------+         |
+|        |10.1.1.1/24        10.1.1.2/24|         |11.1.1.2/24            11.1.1.1/24|          |
++--------+                              +-------+-|                                  +----------+
+
+#endif
+
+
+    graph_t *topo = create_new_graph("3 node linerar topo");
+    node_t *R1 = create_graph_node(topo, "R1");
+    node_t *R2 = create_graph_node(topo, "R2");
+    node_t *R3 = create_graph_node(topo, "R3");
+
+    insert_link_between_two_nodes(R1, R2, "eth0/1", "eth0/2", 1);
+    insert_link_between_two_nodes(R2, R3, "eth0/3", "eth0/4", 1);
+
+    node_set_loopback_address(R1, "122.1.1.1");
+    node_set_intf_ip_address(R1, "eth0/1", "10.1.1.1", 24);
+    
+    node_set_loopback_address(R2, "122.1.1.2");
+    node_set_intf_ip_address(R2, "eth0/2", "10.1.1.2", 24);
+    node_set_intf_ip_address(R2, "eth0/3", "11.1.1.2", 24);
+
+    node_set_loopback_address(R3, "122.1.1.3");
+    node_set_intf_ip_address(R3, "eth0/4", "11.1.1.1", 24);
+
+    network_start_pkt_receiver_thread(topo);
+
+    return topo;
+}
