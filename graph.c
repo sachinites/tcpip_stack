661b6fd3d87ff95214191a731a44e8c53e399b42
sachinites <sachinites@gmail.com>
2019-12-02 13:36:03 -0800

    Memory Corruption fix because of writing into array beyond the array size

--- graph.c
+++ graph.c
@@ -83,7 +83,7 @@ create_new_graph(char *topology_name){
 
     graph_t *graph = calloc(1, sizeof(graph_t));
     strncpy(graph->topology_name, topology_name, 32);
-    graph->topology_name[32] = '\0';
+    graph->topology_name[31] = '\0';
 
     init_glthread(&graph->node_list);
     return graph;
@@ -94,7 +94,7 @@ create_graph_node(graph_t *graph, char *node_name){
 
     node_t *node = calloc(1, sizeof(node_t));
     strncpy(node->node_name, node_name, NODE_NAME_SIZE);
-    node->node_name[NODE_NAME_SIZE] = '\0';
+    node->node_name[NODE_NAME_SIZE - 1] = '\0';
 
     init_udp_socket(node);
 
