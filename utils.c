f0f1803f0a2d36baf4fce0cc2de9cb262d2be5b9
sachinites <sachinites@gmail.com>
2020-04-18 22:06:04 -0700

    Optimized apply_mask fn

--- utils.c
+++ utils.c
@@ -33,6 +33,8 @@
 #include "utils.h"
 #include <sys/socket.h>
 #include <arpa/inet.h> /*for inet_ntop & inet_pton*/
+#include <stdint.h>
+#include <string.h>
 
 /*Apply mask on prefix, and store result in 'str_prefix'
  *For eg : prefix = 122.1.1.1, mask 24, then str_prefix
@@ -41,11 +43,25 @@
 void
 apply_mask(char *prefix, char mask, char *str_prefix){
 
-    unsigned int binary_prefix = 0, i = 0;
+    uint32_t binary_prefix = 0;
+    uint32_t subnet_mask = 0xFFFFFFFF;
+
+    if(mask == 32){
+        strncpy(str_prefix, prefix, 16);
+        str_prefix[15] = '\0';
+        return;
+    }
+    /*Convert Given IP address into binary format*/
     inet_pton(AF_INET, prefix, &binary_prefix);
     binary_prefix = htonl(binary_prefix);
-    for(; i < (32 - mask); i++)
-        UNSET_BIT(binary_prefix, i);
+
+    /*Compute Mask in binary format as well*/
+    subnet_mask = subnet_mask << (32 - mask);
+
+    /*Perform logical AND to apply mask on IP address*/
+    binary_prefix = binary_prefix & subnet_mask;
+
+    /*Convert the Final IP into string format again*/
     binary_prefix = htonl(binary_prefix);
     inet_ntop(AF_INET, &binary_prefix, str_prefix, 16);
     str_prefix[15] = '\0';
