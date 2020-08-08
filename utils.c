5c81cf2c094b5b457eedb170b185c7574e1d67da
sachinites <sachinites@gmail.com>
2020-08-08 13:06:22 -0700

    apply_mask fn improved.

--- utils.c
+++ utils.c
@@ -30,11 +30,11 @@
  * =====================================================================================
  */
 
-#include "utils.h"
 #include <sys/socket.h>
 #include <arpa/inet.h> /*for inet_ntop & inet_pton*/
 #include <stdint.h>
 #include <memory.h>
+#include "utils.h"
 
 /*Apply mask on prefix, and store result in 'str_prefix'
  *For eg : prefix = 122.1.1.1, mask 24, then str_prefix
@@ -43,14 +43,25 @@
 void
 apply_mask(char *prefix, char mask, char *str_prefix){
 
-    uint32_t binary_prefix = 0, i = 0;
-    inet_pton(AF_INET, prefix, &binary_prefix);
-    binary_prefix = htonl(binary_prefix);
-    for(; i < (32 - mask); i++)
-        UNSET_BIT(binary_prefix, i);
-    binary_prefix = htonl(binary_prefix);
-    inet_ntop(AF_INET, &binary_prefix, str_prefix, 16);
-    str_prefix[15] = '\0';
+    uint32_t binary_prefix = 0;
+    uint32_t subnet_mask = ~0;
+
+    if(mask == 32){
+        strncpy(str_prefix, prefix, 16);
+        str_prefix[15] = '\0';
+        return;
+    }
+    /*Convert Given IP address into binary format*/
+    binary_prefix = tcp_ip_covert_ip_p_to_n(prefix);
+
+    /*Compute Mask in binary format as well*/
+    subnet_mask = subnet_mask << (32 - mask);
+
+    /*Perform logical AND to apply mask on IP address*/
+    binary_prefix = binary_prefix & subnet_mask;
+
+    /*Convert the Final IP into string format again*/
+    tcp_ip_covert_ip_n_to_p(binary_prefix, str_prefix);
 }
 
 void
@@ -100,7 +111,7 @@ tlv_buffer_insert_tlv(char *buff, uint8_t tlv_no,
 char *
 tlv_buffer_get_particular_tlv(char *tlv_buff, /*Input TLV Buffer*/
                       uint32_t tlv_buff_size, /*Input TLV Buffer Total Size*/
-                      uint8_t tlv_no,    /*Input TLV Number*/
+                      uint8_t tlv_no,         /*Input TLV Number*/
                       uint8_t *tlv_data_len){ /*Output TLV Data len*/
 
     char tlv_type, tlv_len, *tlv_value = NULL;
@@ -111,6 +122,8 @@ tlv_buffer_get_particular_tlv(char *tlv_buff, /*Input TLV Buffer*/
         *tlv_data_len = tlv_len;
         return tlv_value;
     }ITERATE_TLV_END(tlv_buff, tlv_type, tlv_len, tlv_value, tlv_buff_size); 
+
     *tlv_data_len = 0;
+
     return NULL;
 }
