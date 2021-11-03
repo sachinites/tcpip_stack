93d52a19048ec575d2923e7e11e3b1da69f7bb5d
sachinites <sachinites@gmail.com>
2021-11-03 03:55:34 -0700

    lsp packet printing fn implemented

--- Layer5/isis/isis_lsdb.c
+++ Layer5/isis/isis_lsdb.c
@@ -142,81 +142,96 @@ isis_get_lsp_pkt_seq_no(isis_lsp_pkt_t *lsp_pkt) {
 /* Printing LSP packets */
 
 static uint32_t
-isis_print_formatted_nbr_tlv22(byte *out_buff, 
-                             byte *nbr_tlv_buffer,
-                             uint8_t tlv_buffer_len) {
+ isis_print_formatted_nbr_tlv22 (byte *buff,
+                            byte *nbr_tlv_buffer, 
+                            uint8_t tlv_buffer_len) {
 
     uint32_t rc = 0;
-    uint8_t subtlv_len;
-    byte *subtlv_navigator;
-    unsigned char *ip_addr;
+    byte *subtlv_ptr;
     uint32_t ip_addr_int, metric;
+    uint8_t subtlv_len;
+
     byte tlv_type, tlv_len, *tlv_value = NULL;
 
-    ITERATE_TLV_BEGIN(nbr_tlv_buffer, tlv_type,
-                        tlv_len, tlv_value, tlv_buffer_len) {
+    ITERATE_TLV_BEGIN(nbr_tlv_buffer, tlv_type, tlv_len, tlv_value, tlv_buffer_len) {
 
-        rc += sprintf(out_buff + rc,
-                      "\tTLV%d  Len : %d\n", tlv_type, tlv_len);
-
+        if (buff) {
+        rc += sprintf (buff + rc , 
+                    "\tTLV%d  Len : %d\n", tlv_type, tlv_len);
+        }
+        else {
+            rc += printf( "\tTLV%d  Len : %d\n", tlv_type, tlv_len);
+        }
+        /* Now we shall extract IP Addr, metric and subtlv len */
         ip_addr_int = *(uint32_t *)tlv_value;
         metric = *(uint32_t *)(((uint32_t *)tlv_value) + 1);
         subtlv_len = *(uint8_t *)((uint32_t *)tlv_value + 2);
 
-        rc += sprintf(out_buff + rc, "\t\tNbr Rtr ID : %s   Metric : %u   SubTLV Len : %d\n",
-                      tcp_ip_covert_ip_n_to_p(ip_addr_int, 0),
-                      metric, subtlv_len);
-
-        subtlv_navigator = tlv_value + 
-                            sizeof(uint32_t) +  // 4B IP Addr
-                            sizeof(uint32_t) +  // 4B metric
-                            sizeof(uint8_t);    // 1B subtlv len
+        if (buff) {
+        rc += sprintf(buff + rc, "\t\tNbr Rtr ID : %s     metric : %u    SubTLV len : %d\n",
+                      tcp_ip_covert_ip_n_to_p(ip_addr_int, 0), metric, subtlv_len);
+        }
+        else {
+            rc += printf ("\t\tNbr Rtr ID : %s     metric : %u    SubTLV len : %d\n",
+                      tcp_ip_covert_ip_n_to_p(ip_addr_int, 0), metric, subtlv_len);
+        }
+        subtlv_ptr = tlv_value + 
+                            sizeof (uint32_t) +    // 4B of IP Addr ( nbr lo addr )
+                            sizeof(uint32_t) +     // 4B of metric  
+                            sizeof(uint8_t);         // 1B of subtlv len
 
         /* Now Read the Sub TLVs */
         byte tlv_type2, tlv_len2, *tlv_value2 = NULL;
 
-        ITERATE_TLV_BEGIN(subtlv_navigator, tlv_type2,
-                        tlv_len2, tlv_value2, subtlv_len) {
+        ITERATE_TLV_BEGIN (subtlv_ptr, tlv_type2, tlv_len2, tlv_value2, subtlv_len) {
 
-            switch(tlv_type2) {
+            switch (tlv_type2) {
                 case ISIS_TLV_IF_INDEX:
-
-                    rc += sprintf(out_buff + rc,
-                                  "\tSubTLV%d  Len : %d   if-indexes [local : %u, remote : %u]\n",
-                                  tlv_type2, tlv_len2,
-                                  *(uint32_t *)tlv_value2,
-                                  *(uint32_t *)((uint32_t *)tlv_value2 + 1));
-
-                    break;
+                    if (buff) {
+                    rc += sprintf (buff + rc, 
+                                "\tSubTLV%d  Len : %d   if-indexes [local : %u, remote : %u]\n",
+                                tlv_type2, tlv_len2, *(uint32_t *)tlv_value2, 
+                                *(uint32_t *)((uint32_t *)tlv_value2 + 1));
+                    }
+                    else {
+                        rc += printf ( "\tSubTLV%d  Len : %d   if-indexes [local : %u, remote : %u]\n",
+                                tlv_type2, tlv_len2, *(uint32_t *)tlv_value2, 
+                                *(uint32_t *)((uint32_t *)tlv_value2 + 1));
+                    }
+                        break;
                 case ISIS_TLV_LOCAL_IP:
                     ip_addr_int = *(uint32_t *)tlv_value2;
-
-                    rc += sprintf(out_buff + rc,
-                                  "\tSubTLV%d  Len : %d   Local IP : %s\n",
-                                  tlv_type2, tlv_len2,
-                                  tcp_ip_covert_ip_n_to_p(ip_addr_int, 0));
-
-                    break;
+                    if (buff) {
+                    rc += sprintf (buff + rc , "\tSubTLV%d  Len : %d  Local IP : %s\n",
+                                tlv_type2, tlv_len2, 
+                                tcp_ip_covert_ip_n_to_p(ip_addr_int, 0));
+                    }
+                    else {
+                        rc += printf ("\tSubTLV%d  Len : %d  Local IP : %s\n",
+                                tlv_type2, tlv_len2, 
+                                tcp_ip_covert_ip_n_to_p(ip_addr_int, 0));
+                    }
+                        break;
                 case ISIS_TLV_REMOTE_IP:
                     ip_addr_int = *(uint32_t *)tlv_value2;
-
-                    rc += sprintf(out_buff + rc,
-                                  "\tSubTLV%d  Len : %d   Remote IP : %s\n",
-                                  tlv_type2, tlv_len2,
-                                  tcp_ip_covert_ip_n_to_p(ip_addr_int, 0));
-
-                    break;
-                default:
-                    ;
+                    if (buff) {
+                    rc += sprintf (buff + rc , "\tSubTLV%d  Len : %d  Remote IP : %s\n",
+                                tlv_type2, tlv_len2, 
+                                tcp_ip_covert_ip_n_to_p(ip_addr_int, 0));
+                    }
+                    else {
+                        rc += printf ("\tSubTLV%d  Len : %d  Remote IP : %s\n",
+                                tlv_type2, tlv_len2, 
+                                tcp_ip_covert_ip_n_to_p(ip_addr_int, 0));
+                    }
+                        break;
+                default: ;
             }
-
-        } ITERATE_TLV_END(subtlv_navigator, tlv_type2,
-                        tlv_len2, tlv_value2, subtlv_len);
- 
-    } ITERATE_TLV_END(nbr_tlv_buffer, tlv_type,
-                        tlv_len, tlv_value, tlv_buffer_len);
+        } ITERATE_TLV_END (subtlv_ptr, tlv_type2, tlv_len2, tlv_value2, subtlv_len);
+    }ITERATE_TLV_END(nbr_tlv_buffer, tlv_type, tlv_len, tlv_value, tlv_buffer_len);
     return rc;
 }
+
 
 uint32_t 
 isis_show_one_lsp_pkt_detail (byte *buff, 
@@ -225,41 +240,67 @@ isis_show_one_lsp_pkt_detail (byte *buff,
 
     uint32_t rc = 0;
     isis_lsp_pkt_t *lsp_pkt;
-    
+
     byte tlv_type, tlv_len, *tlv_value = NULL;
 
-    rc += sprintf(buff + rc, "LSP : %s(%u)\n",
-             tcp_ip_covert_ip_n_to_p(lsp_pkt_hdr->rtr_id, 0), 
-            lsp_pkt_hdr->seq_no);
+    if (buff) {
+    rc += sprintf(buff + rc , "LSP L %s(%u)\n", 
+                            tcp_ip_covert_ip_n_to_p(lsp_pkt_hdr->rtr_id, 0),
+                            lsp_pkt_hdr->seq_no);
+    }
+    else {
+        rc += printf ( "LSP L %s(%u)\n", 
+                            tcp_ip_covert_ip_n_to_p(lsp_pkt_hdr->rtr_id, 0),
+                            lsp_pkt_hdr->seq_no);
+    }
 
-    rc += sprintf(buff + rc,  "Flags : 0x%x\n", lsp_pkt_hdr->flags);
-    
-    rc += sprintf(buff + rc, "TLVs\n");
+    if (buff) {
+    rc += sprintf (buff + rc , "Flags : 0x%x\n", lsp_pkt_hdr->flags);
+    }
+    else {
+        rc += printf ("Flags : 0x%x\n", lsp_pkt_hdr->flags);
+    }
+
+    if (buff) {
+    rc += sprintf(buff + rc , "TLVs\n");
+    }
+    else {
+        rc += printf ("TLVs\n");
+    }
+
+
+
 
     byte *lsp_tlv_buffer = (byte *)(lsp_pkt_hdr + 1);
-    uint16_t lsp_tlv_buffer_size = (uint16_t)(lsp_pkt->pkt_size -
-                                        ETH_HDR_SIZE_EXCL_PAYLOAD -
-                                        sizeof(isis_pkt_hdr_t)) ;
+    uint16_t lsp_tlv_buffer_size = (uint16_t) (lsp_pkt->pkt_size -
+                                                    ETH_HDR_SIZE_EXCL_PAYLOAD -
+                                                    sizeof(isis_pkt_hdr_t));
 
-    ITERATE_TLV_BEGIN(lsp_tlv_buffer, tlv_type,
-                        tlv_len, tlv_value,
-                        lsp_tlv_buffer_size) {
+    ITERATE_TLV_BEGIN (lsp_tlv_buffer, tlv_type, 
+                                                tlv_len, tlv_value,
+                                                lsp_tlv_buffer_size) {
 
         switch(tlv_type) {
+
             case ISIS_TLV_HOSTNAME:
-                rc += sprintf(buff + rc,  "\tTLV%d Host-Name : %s\n", 
-                        tlv_type, tlv_value);
+                if (buff) {
+                rc += sprintf (buff + rc , "\tTLV%d Host-Name : %s\n", 
+                            tlv_type, tlv_value);
+                }
+                else {
+                    rc += printf ( "\tTLV%d Host-Name : %s\n",  tlv_type, tlv_value);
+                }
             break;
             case ISIS_IS_REACH_TLV:
-                 rc += isis_print_formatted_nbr_tlv22( buff + rc,
-                        tlv_value - TLV_OVERHEAD_SIZE,
-                        tlv_len + TLV_OVERHEAD_SIZE);
-                break;
-            default: ;
+                rc += isis_print_formatted_nbr_tlv22(buff + rc ,
+                                tlv_value - TLV_OVERHEAD_SIZE,
+                                tlv_len + TLV_OVERHEAD_SIZE);
+            break;
+            default:;
         }
-    } ITERATE_TLV_END(lsp_tlv_buffer, tlv_type,
-                        tlv_len, tlv_value,
-                        lsp_tlv_buffer_size);
 
+    } ITERATE_TLV_END (lsp_tlv_buffer, tlv_type, 
+                                                tlv_len, tlv_value,
+                                                lsp_tlv_buffer_size); 
     return rc;
 }
